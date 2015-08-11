ea08a0693490f002d927b7210fef3b78e0c98f25
Jeffrey Cassidy <jeffrey.cassidy@gmail.com>
2015-08-11 18:29:45 -0400

    Fixed bad init that was causing Boost to assert() due to null mpz_t contents during Bluesim simulation

--- BitPackUnpack/ConstGMPArray.hpp
+++ BitPackUnpack/ConstGMPArray.hpp
@@ -20,32 +20,44 @@ class ConstGMPArray
 public:
 	ConstGMPArray(unsigned bits,const uint32_t* p)
 	{
+		mpz_t *t = &x_.backend().data();
+
 		// calculate number of GMP limbs in the value
 		std::size_t Nl = bits==0 ? 0 : (bits+(sizeof(mp_limb_t)<<3)-1)/(sizeof(mp_limb_t)<<3);
 
-		// Setup the GMP int to point to the already-existing uint32_t* rather than allocating new
-		mpz_t *t = &x_.backend().data();
-		t[0]->_mp_alloc=t[0]->_mp_size=Nl;
-		t[0]->_mp_d=(reinterpret_cast<mp_limb_t*>((uint32_t*)p));			// cast away constness
+		if (p)
+		{
+			assert(bits);
+
+			// Setup the GMP int to point to the already-existing uint32_t* rather than allocating new
+			t[0]->_mp_alloc=t[0]->_mp_size=Nl;
+			t[0]->_mp_d=(reinterpret_cast<mp_limb_t*>((uint32_t*)p));			// cast away constness
+		}
+		else
+			mpz_init(*t);
 	}
 
 	// release the storage; we do not actually own the memory so should not deallocate it (Boost destructor will automatically)
 	~ConstGMPArray()
 	{
-		x_.backend().data()->_mp_alloc=0;
+		x_.backend().data()->_mp_alloc=x_.backend().data()->_mp_size=0;
 		x_.backend().data()->_mp_d=nullptr;
 	}
 
 	unsigned bits() const { return bits_; }
 
-	const boost::multiprecision::number<boost::multiprecision::gmp_int>& value() const { return x_; }
+	const boost::multiprecision::number<boost::multiprecision::gmp_int>& value() const
+			{ assert(x_.backend().data()[0]._mp_d); return x_; }
 
 private:
 	boost::multiprecision::number<boost::multiprecision::gmp_int> x_;
 	unsigned bits_=0;
 };
 
+template<class Output,class BitVector>Output bitslice(const BitVector&,unsigned,unsigned);
+
 template<class Output>Output bitslice(const ConstGMPArray& v,unsigned from,unsigned downto)
-	{ return bitslice<Output>(v.value(),from,downto); }
+	{ return bitslice<Output> (v.value(),from,downto); }
+
 
 #endif /* BITPACKUNPACK_CONSTGMPARRAY_HPP_ */
